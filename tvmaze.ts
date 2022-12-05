f689217777d45e748737cc8c3e6e0f31a0ebea8d
Spencer Taira <spencermtaira@gmail.com>
2022-12-05 12:50:26 -0800

    works with strict set to true

--- tvmaze.ts
+++ tvmaze.ts
@@ -1,10 +1,27 @@
 import axios from "axios";
 import * as $ from 'jquery';
 
-const $showsList = $("#showsList");
-const $episodesArea = $("#episodesArea");
-const $searchForm = $("#searchForm");
+const DEFAULT_IMAGE_URL: string = "https://cdn.pixabay.com/photo/2022/11/19/09/35/forest-7601671_960_720.jpg";
+const BASE_SHOW_API_URL: string = "http://api.tvmaze.com/search/shows";
+const BASE_EPISODE_API_URL: string = "http://api.tvmaze.com/shows";
+const $showsList: JQuery<HTMLElement> = $("#showsList");
+const $episodesArea: JQuery<HTMLElement> = $("#episodesArea");
+const $episodeList: JQuery<HTMLElement> = $("#episodesList");
+const $searchForm: JQuery<HTMLElement> = $("#searchForm");
 
+interface ShowInterface {
+  id: number;
+  name: string;
+  summary: string;
+  image: string;
+}
+
+interface EpisodeInterface {
+  id: number;
+  name: string;
+  season: number;
+  number: number;
+}
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -13,40 +30,37 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+async function getShowsByTerm(term: string): Promise<Array<ShowInterface>> {
+
+  let response = await axios.get(
+    BASE_SHOW_API_URL,
+    {params: {q: term}}
+  );
+
+  console.log('RESPONSE', response);
+
+  const shows: Array<ShowInterface> = response.data.map(s => ({
+    id: s.show.id,
+    name: s.show.name,
+    summary: s.show.summary,
+    image: s.show.image.medium || DEFAULT_IMAGE_URL
+  }));
+
+  return shows;
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows(shows) {
+function populateShows(shows: Array<ShowInterface>): void {
   $showsList.empty();
 
   for (let show of shows) {
-    const $show = $(
+    const $show: JQuery<HTMLElement> = $(
         `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              src=${show.image}
               alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
@@ -60,7 +74,8 @@ function populateShows(shows) {
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
@@ -68,15 +83,15 @@ function populateShows(shows) {
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
 
-async function searchForShowAndDisplay() {
-  const term = $("#searchForm-term").val();
+async function searchForShowAndDisplay(): Promise<void> {
+  const term = $("#searchForm-term").val() as string;
   const shows = await getShowsByTerm(term);
 
   $episodesArea.hide();
   populateShows(shows);
 }
 
-$searchForm.on("submit", async function (evt) {
+$searchForm.on("submit", async function (evt: JQuery.SubmitEvent): Promise<void> {
   evt.preventDefault();
   await searchForShowAndDisplay();
 });
@@ -86,8 +101,50 @@ $searchForm.on("submit", async function (evt) {
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function searchForEpisodesAndDisplay(id: string): Promise<void> {
+  const episodes = await getEpisodesOfShow(id);
+  populateEpisodes(episodes);
+}
+
+
+async function getEpisodesOfShow(id: string): Promise<Array<EpisodeInterface>> {
+  let response = await axios.get(
+    `${BASE_EPISODE_API_URL}/${id}/episodes)`
+  );
+
+  const episodes: Array<EpisodeInterface> = response.data.map(e => ({
+    id: e.id,
+    name: e.name,
+    season: e.season,
+    number: e.number
+  }));
+
+  return episodes;
+}
 
 /** Write a clear docstring for this function... */
 
-// function populateEpisodes(episodes) { }
+function populateEpisodes(episodes: Array<EpisodeInterface>): void{
+  $episodesArea.show();
+
+  for (const episode of episodes) {
+    const $episode: JQuery<HTMLElement> = $(
+      `
+        <li>
+          <h5>${episode.name}</h5>
+          <p>Season: ${episode.season}</p>
+          <p>Episode: ${episode.number}</p>
+        </li>
+      `
+    )
+    $episodeList.append($episode);
+  }
+}
+
+$showsList.on("click", async function (evt: JQuery.ClickEvent): Promise<void> {
+  console.log('button click');
+  evt.preventDefault();
+  const id: string = $(evt.target).closest('.Show').attr('data-show-id');
+  console.log('SHOW ID', id);
+  await searchForEpisodesAndDisplay(id);
+});
